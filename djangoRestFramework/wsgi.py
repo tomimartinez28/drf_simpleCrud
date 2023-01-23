991801c16fa343a6bf5b2cb5e4e74fa613999147
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2023-01-23 09:56:51 -0300

    wsgi

--- djangoRestFramework/wsgi.py
+++ djangoRestFramework/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoRestFramework.djangoRestFramework.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoRestFramework.settings')
 
 application = get_wsgi_application()
